78a1416935de5a88210b1ea2d29ec9ba31b0f48d
satish <sgaud2001@gmail.com>
2020-12-03 22:30:29 -0800

    develop

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ import express from "express"
 const app=express()
 
 app.use((req,res,next)=>{
-    res.send("Developer Backend running...")
+    res.send("developer Backend running...")
 })
 
 export default app
